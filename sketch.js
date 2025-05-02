b572c9c9af14d8f482e0b81f7662e53b9833ce47
lettin0622 <appleyoonki@gmail.com>
2025-05-02 11:42:30 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -20,9 +20,11 @@ function draw() {
   for (let x = 0; x < capture.width; x += 20) {
     for (let y = 0; y < capture.height; y += 20) {
       let col = capture.get(x, y); // 取得 capture 對應位置的顏色
-      graphics.fill(col); // 設定圓的顏色
-      graphics.noStroke(); // 移除圓的邊框
-      graphics.ellipse(x + 10, y + 10, 15, 15); // 繪製圓，中心點偏移 10 以對齊單位格
+      graphics.fill(col); // 設定方框的顏色
+      graphics.noStroke(); // 移除邊框
+      graphics.rect(x + 2.5, y + 2.5, 15, 15); // 繪製方框，偏移 2.5 以對齊單位格
+      graphics.fill(0); // 設定圓的顏色為黑色
+      graphics.ellipse(x + 10, y + 10, 5, 5); // 在方框中心繪製圓
     }
   }
 
