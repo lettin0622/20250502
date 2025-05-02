a800fcf9a3557bdc4aa2fa724e292b7448e401b7
lettin0622 <appleyoonki@gmail.com>
2025-05-02 10:24:52 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,6 +10,8 @@ function setup() {
 
 function draw() {
   background('#fefae0'); // 確保背景顏色一致
+  translate(width, 0); // 將畫布的原點移到右上角
+  scale(-1, 1); // 水平翻轉畫布
   let x = (width - capture.width) / 2; // 計算影像的水平居中位置
   let y = (height - capture.height) / 2; // 計算影像的垂直居中位置
   image(capture, x, y, capture.width, capture.height); // 在畫布中央顯示影像
